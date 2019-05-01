160d29b522a2126d5356eed06c623e7c44efe1a6
Darkangel <wtsegars@mac.com>
2019-05-01 13:27:51 -0400

    app/

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+
+module.exports = function(app, path) {
+
+    app.get('api/friends', function(req, res) {
+        fs.readFile("app/data/friends.js", "utf8", function(err, data) {
+            if (err) throw err;
+
+            else {
+                res.json(JSON.parse(data));
+            }
+        });
+    });
+
+    app.post('/api/friends', function(req, res) {
+        let results = [];
+
+        const postResponse = JSON.stringify(req.body);
+
+        fs.readFile('app/data/friends.js', function (err, data) {
+            const friendFile = JSON.parse(data);
+
+            let closestMatch = 0;
+            let matchScore = 999999999999999;
+
+            for (let i = 0; i < friendFile.length; i++) {
+                let spaceBetween = 0;
+                for (let j = 0; j < friendFile[i]['answers[]'].length; j++) {
+                    spaceBetween += Math.abs((parseInt(req.body['answers[]'][j]) - parseInt(friendFile[i]['answers[]'][j])));
+                }
+                if (spaceBetween <= matchScore) {
+                    matchScore = spaceBetween;
+                    closestMatch == i;
+                } 
+            }
+
+            results.push(friendFile[closestMatch]);
+
+            friendFile.push(JSON.parse(postResponse));
+
+            fs.writeFile("app/data/friends.js", JSON.stringify(friendFile));
+                res.send(results[0]);
+        })
+    })
+}
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,2 +1,9 @@
-const app = express();
-const PORT = 3000;
+module.exports = function(app, path) {
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+    
+    app.get("/survey", function(req, res) {
+        res.sendFile(path.join(__dirname, "..public/survey.html"));
+    });
+}
