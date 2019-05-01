082ef5c1967f226b501a7d176672ef53f81f21fc
Darkangel <wtsegars@mac.com>
2019-05-01 13:28:13 -0400

    server.js

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const PORT = 3000;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.use(express.static('app/public'));
+
+require('./app/routing/apiRoutes.js')(app, path);
+require('./app/routing/htmlRoutes.js')(app, path);
+
+app.listen(PORT, function() {
+    console.log("Listening on port..." + PORT);
+});
