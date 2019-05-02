758b95578300f86defb0f9b36c528e664f849cc8
Darkangel <wtsegars@mac.com>
2019-05-01 21:26:51 -0400

    server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const fs = require('fs');
 
 const app = express();
-const PORT = 3000;
+const PORT = 8080;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
