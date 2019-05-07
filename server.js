7a87abcdac77ab16defdbd5d1b12df6ce6b0a5ec
Darkangel <wtsegars@mac.com>
2019-05-06 20:35:32 -0400

    hello-world2

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const fs = require('fs');
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
